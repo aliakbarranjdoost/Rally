44ad025f0ce1aa3eab2d3820feebea4912fc3299
AliAkbar <aliakbarranjdoost@gmail.com>
2023-11-25 13:46:40 +0330

    Pathway 3 - Activity 7 - Part 8. Enable deep link support

--- app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -110,7 +110,8 @@ fun RallyApp() {
                 }
                 composable(
                     route = SingleAccount.routeWithArgs,
-                    arguments = SingleAccount.arguments
+                    arguments = SingleAccount.arguments,
+                    deepLinks = SingleAccount.deepLinks
                 ) { navBackStackEntry ->
                     // Retrieve the passed argument
                     val accountType =
